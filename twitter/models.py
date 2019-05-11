70a4e568f010a08688d2e5c0d50494cd09c53280
MatBrze <mateusz.brzezny@gmail.com>
2019-05-11 15:32:48 +0200

    first part of exercises 3 model implemented migrations generated and applied

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+TWITTER_MAXIMUM_TWEET_LENGTH = 280
+TWITTER_MAXIMUM_COMMENT_LENGTH = 60
+TWITTER_MAXIMUM_MSG_LENGTH = 64
+
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=TWITTER_MAXIMUM_TWEET_LENGTH)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '[{}] TWEET by {}: {}'.format(
+            self.creation_date,
+            self.author, self.content[:20])
