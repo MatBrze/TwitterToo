91d7090019c307be094b176922a2913c5e3d0f0c
MatBrze <mateusz.brzezny@gmail.com>
2019-06-30 17:59:15 +0200

    Message and Comment models created

--- twitter/models.py
+++ twitter/models.py
@@ -18,3 +18,29 @@ class Tweet(models.Model):
         return '[{}] TWEET by {}: {}'.format(
             self.creation_date,
             self.author, self.content[:20])
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=TWITTER_MAXIMUM_COMMENT_LENGTH)
+    date_comment = models.DateTimeField(auto_now_add=True)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+
+class Message(models.Model):
+    title = models.CharField(max_length=TWITTER_MAXIMUM_MSG_LENGTH)
+    content = models.TextField()
+    date_send = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="user_from")
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE,
+                                  related_name="user_to")
+    read = models.BooleanField(default=False)
+    blocked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'from: {self.sender} to: ' \
+               f'{self.recipient} message: {self.content[0:20]}'
