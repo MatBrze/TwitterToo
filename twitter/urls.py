013fcbfa234814cc315ac387db8aae5079c59545
MatBrze <mateusz.brzezny@gmail.com>
2019-06-30 18:39:38 +0200

    user detail template and url added

--- twitter/urls.py
+++ twitter/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from twitter import views
-from twitter.views import RegisterView
 
 app_name = 'twitter'
 
@@ -11,8 +10,10 @@ urlpatterns = [
     path('compose/', views.TweetComposeView.as_view(), name="compose"),
     path('login/', auth_views.LoginView.as_view(template_name='twitter/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='twitter/logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('tweet/<int:pk>/', views.TweetDetailView.as_view(),
          name='tweet-detail'),
+    path('user/<int:pk>/', views.AuthorDetailView.as_view(),
+         name='author-detail'),
 ]
